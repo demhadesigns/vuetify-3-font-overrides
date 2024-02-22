66c38c768e879cf0c9953710ae5e59e40b458d6c
Ahmed El Siblani <ahmed_siblani@hotmail.de>
2024-02-22 18:37:22 +0100

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <h1 class="text-body">Overriding fonts in vuetify</h1>
+      <br/>
+      <h1 class="text-h1">text-h1 uses font "AURUM"</h1>
+      <br/>
+      <h2 class="text-h2">text-h2 uses font "CHUNKY"</h2>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,18 @@
+/**
+ * plugins/vuetify.js
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import '@/styles/main.scss'
+import 'vuetify/styles'
+
+// Composables
+import { createVuetify } from 'vuetify'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  //
+})
